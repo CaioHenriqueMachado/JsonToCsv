0a42380e6c49231101709193149276778e8bf7d1
Caio <caio.student@outlook.com>
2020-08-17 09:25:22 -0300

    ajustando convert

--- js/script.js
+++ js/script.js
@@ -5,53 +5,58 @@ var result = document.getElementById('result');
 
 
 function JSON2CSV() {
-  var csv = '';
+  var json = '';
   var index = [];
-  var toJson = [];
+  var toCsv = [];
   var stringJson = '[\n';
   var valid = false;
 
   var texto = String(text.value);
 
   for ( i=0; i<texto.length; i++) {
+
     if (texto[i] == "'"  || texto[i] == '"' || texto[i] == '[' || texto[i] == ']' || texto[i] == '}' || texto[i] == '{') {
       valid = true;
     }
-    if (texto[i] == "\n") {
-      csv += '|||';
+    if (texto[i] == "\n" || texto[i] == " " || texto[i] == "[" || texto[i] == "]") {
+
     } else {
-      csv += texto[i];
+      json += texto[i];
     }
  }
 
- index = csv.split('|||');
+ index = json.split('},{');
+
 
   for (var i = 0; i < index.length; i++) {
-    toJson.push(index[i].split(','));
+    // DAR UM REPLACE DE : PARA ,
+    toCsv.push(index[i].split(','));
+    toCsv.push(index[i].split(':'));
   }
+  console.log(toCsv);
 
-  for (var i = 1; i < toJson.length; i++) {
-    stringJson +='{';
+//   for (var i = 1; i < toJson.length; i++) {
+//     stringJson +='{';
 
-    for (var j = 0; j < toJson[0].length; j++) {
-      stringJson += '"' + toJson[0][j] + '":';
-      stringJson += '"' + toJson[i][j] + '"';
-      if (j != toJson[0].length - 1){
-        stringJson += ',';
-      }
-    }
+//     for (var j = 0; j < toJson[0].length; j++) {
+//       stringJson += '"' + toJson[0][j] + '":';
+//       stringJson += '"' + toJson[i][j] + '"';
+//       if (j != toJson[0].length - 1){
+//         stringJson += ',';
+//       }
+//     }
 
-    if (i != toJson.length - 1){
-      stringJson += '},\n';
-    } else {
-      stringJson += '}\n]';
-    }
-  }
+//     if (i != toJson.length - 1){
+//       stringJson += '},\n';
+//     } else {
+//       stringJson += '}\n]';
+//     }
+//   }
 
   if (valid == false){
     result.value = "INPUT INVALID !!"
   } else {
-    result.value = stringJson;
+    result.value = index;
   }
 }
 
