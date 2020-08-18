ba734eba891b06ae590d096707fe0691341b5f7a
Caio <caio.student@outlook.com>
2020-08-18 19:19:52 -0300

    configuração para inserir arquivo

--- js/script.js
+++ js/script.js
@@ -116,3 +116,25 @@ function CSV2JSON() {
     result.value = stringJson;
   }
 }
+
+
+//  READ FILE
+
+document.getElementById('file').onchange = function(){
+
+  var file = this.files[0];
+
+  var reader = new FileReader();
+  reader.onload = function(progressEvent){
+    // Entire file
+    console.log(this.result);
+
+    // By lines
+    var lines = this.result;
+    // for(var line = 0; line < lines.length; line++){
+    //   console.log(lines[line]);
+    // }
+    text.value = lines;
+  };
+  reader.readAsText(file);
+};
